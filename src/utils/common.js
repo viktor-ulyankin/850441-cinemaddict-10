e70af5b200d683cdbf9a36a836d39b30b54119c8
Viktor Ulyankin <freeman.viktor@mail.ru>
2020-01-03 22:43:03 +0500

    Исправление ошибок 2.

--- src/utils/common.js
+++ src/utils/common.js
@@ -12,7 +12,6 @@ export const replace = (newComponent, oldComponent) => {
     const parentElement = oldComponent.getElement().parentElement;
     const newElement = newComponent.getElement();
     const oldElement = oldComponent.getElement();
-
     const isExistElements = !!(parentElement && newElement && oldElement);
 
     if (isExistElements && parentElement.contains(oldElement)) {
